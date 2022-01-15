2474c680d9c52200d0801258d584266eabc5fd59
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-01-15 01:32:14 +0100

    setup client-ts

---
+++ packages/client-ts/src/App.tsx
@@ -0,0 +1,37 @@
+import { ChevronRightBold } from '@pretto/picto'
+import { useState } from 'react'
+
+import './fonts/MaisonNeue.css'
+
+import ButtonSquare from './components/ButtonSquare'
+import Checkbox from './components/Checkbox'
+import TextField from './components/TextField'
+
+import Theme from './components/Theme'
+
+export default function App() {
+  const [isChecked, setIsChecked] = useState(false)
+  const [textFieldValue, setTextFieldValue] = useState('')
+
+  const handleCheckboxChange = () => {
+    setIsChecked((isChecked) => !isChecked)
+  }
+
+  const handleTextFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTextFieldValue(event.currentTarget.value)
+  }
+
+  return (
+    <Theme>
+      <div>
+        <TextField onChange={handleTextFieldChange} placeholder="Search..." value={textFieldValue} />
+
+        <ButtonSquare>
+          <ChevronRightBold />
+        </ButtonSquare>
+
+        <Checkbox isChecked={isChecked} onChange={handleCheckboxChange} />
+      </div>
+    </Theme>
+  )
+}
